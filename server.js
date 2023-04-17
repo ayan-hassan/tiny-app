b73f251617bc9d41f07a01d51d1c6b86a1fa5942
ayan-hassan <ahassan@hotmail.ca>
2023-04-17 16:18:59 -0400

    implement parsebody, random char generator

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(3, 9);
 };
 
 app.get("/", (req, res) => {
@@ -30,6 +36,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("ok");
 });
 
 app.get("/urls/:id", (req, res) => {
