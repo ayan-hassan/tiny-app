df07b0f1cc90647f644ed6b3423a8650fa41d11b
ayan-hassan <ahassan@hotmail.ca>
2023-04-17 17:11:50 -0400

    add post to redirect after form submit & 2 longURL

--- server.js
+++ server.js
@@ -39,8 +39,6 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  // console.log(req.body);
-  // res.send("ok");
   let longURL = req.body.longURL;
   let tinyURL = generateRandomString();
   urlDatabase[tinyURL] = longURL;
@@ -50,6 +48,11 @@ app.post("/urls", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 
