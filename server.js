92e18091d8e4a1627df20bb307f7e6315932baf0
ayan-hassan <ahassan@hotmail.ca>
2023-04-17 19:17:43 -0400

    install cookie-parser, display username in header

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,12 +33,18 @@ app.get("/hello", (req, res) => {
 //------------------------ROUTES-----------------------------//
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 //displays all saved urls
@@ -48,7 +57,10 @@ app.post("/urls", (req, res) => {
 
 //single url page
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -72,9 +84,18 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase,
+  };
+  res.render("urls_index", templateVars);
+});
+
 //login with username
 app.post("/login", (req, res) => {
-  let username = req.body.username;
+  const username = req.body.username;
   res.cookie('username', username);
   res.redirect("/urls");
 });
