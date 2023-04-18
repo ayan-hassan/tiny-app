33394449a3aa378adfa2d3945bd49bdf9cb8693e
ayan-hassan <ahassan@hotmail.ca>
2023-04-18 19:26:09 -0400

    remove users and urlDatabase test code

--- server.js
+++ server.js
@@ -9,29 +9,9 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser());
 
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW",
-  },
-};
+const urlDatabase = {};
 
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  }
-};
+const users = {};
 
 //---------------- HELPER FUNCTIONS -------------------//
 
